d73fbaaf618d8edc5cd9a04cdcdb350443b6b76d
Chibuike Ilo <chibuike.ilo@decagon.dev>
2023-10-31 13:08:38 +0100

    added typorem config for postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService, ConfigModule } from '@nestjs/config';
 import { join } from 'path';
 import { BookModule } from './book/book.module';
 import { UserModule } from './user/user.module';
@@ -13,6 +15,16 @@ import { UserModule } from './user/user.module';
     }),
     BookModule,
     UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '!2012Chisom',
+      database: 'gql-ex',
+      entities: ['dist/**/*.entity.js'],
+      synchronize: true,
+    }),
   ],
 })
 export class AppModule {}
